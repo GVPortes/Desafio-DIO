9f7c703a4219e5695d95dfc82813352ccc5222a6
Gabriel <gvportes@gmail.com>
2024-01-06 02:01:26 -0300

    teste

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("oi");
+let hero = "Super Gabz ";
+let xp = "10000";
+
+if (xp < 1000){
+    console.log(hero + "é nível Ferro")
+}
+else if (xp >= 1000 && xp < 2000){console.log(hero + "é nível Bronze")
+}
+else if (xp >= 2000 && xp < 5000){console.log(hero + "é nível Prata")
+}
+else if (xp >= 5000 && xp < 7000){console.log(hero + "é nível Ouro")
+}
+else if (xp >= 7000 && xp < 8000){console.log(hero + "é nível Platina")
+}
+else if (xp >= 8000 && xp < 9000){console.log(hero + "é nível Ascendental")
+}
+else if (xp >= 9000 && xp < 10000){console.log(hero + "é nível Imortal")
+}
+else {console.log(hero + "é nível Radiante")
+}
